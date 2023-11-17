7eab7950ca26849e9b4b6661a9ddae27d89bc71f
Gayle Martin <hello.gmartin@gmail.com>
2023-11-17 17:27:52 -0600

    Fix url paths

--- src/components/Presentations.jsx
+++ src/components/Presentations.jsx
@@ -8,7 +8,7 @@ function Presentations() {
         <Title>Presentations</Title>
         <p className='pb-2 pt-2'>"Apache Kafka Explained" | Tech Talk sponsored by Remake Labs - October 2023</p>
         <div className='py-5 text-center' style={{ position: 'relative', paddingBottom: '56.25%' }}>
-          <iframe src='/portfolio/assets/trimmed-tech-talk.mp4?autoplay=0' width='100%' height='100%' style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }} frameBorder='0' allowFullScreen='' title='Embedded post'></iframe>
+          <iframe src='/assets/trimmed-tech-talk.mp4?autoplay=0' width='100%' height='100%' style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }} frameBorder='0' allowFullScreen='' title='Embedded post'></iframe>
         </div>
       </div>
     </div>
