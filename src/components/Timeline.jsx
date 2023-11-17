f735f7fefa474b0e32297b1e4604fc6df6f23d0b
Gayle Martin <hello.gmartin@gmail.com>
2023-11-17 10:50:09 -0600

    Update timeline content

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -7,7 +7,7 @@ function Timeline() {
   return (
     <div className='flex flex-col md:flex-row items-center justify-center my-20'>
       <div className='w-full md:w-7/12'>
-        <Title>Timeline</Title>
+        <Title>My Story</Title>
         {timeline.map(item => (
           <TimelineItem year={item.year} title={item.title} duration={item.duration} details={item.details} />
         ))}
--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -8,9 +8,9 @@ function TimelineItem({ year, title, duration, details }) {
         <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
           <span className='inline-block px-2 py-1 font-semibold text-white dark:text-stone-900 bg-stone-900 dark:bg-white rounded-md'>{year}</span>
           <h3 className='text-lg font-semibold text-stone-900 dark:text-white'>{title}</h3>
-          <div className='my-1 text-sm font-normal leading-none text-stone-400 dark:text-stone-500'>{duration}</div>
+          <div className='my-1 text-sm font-normal leading-none text-stone-500 dark:text-stone-400'>{duration}</div>
         </p>
-        <p className='my-2 text-base font-normal text-stone-500 dark:text-stone-400'>{details}</p>
+        <p className='my-2 text-base font-normal text-stone-700 dark:text-stone-300'>{details}</p>
       </li>
     </ol>
   );
