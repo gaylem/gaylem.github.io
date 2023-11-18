6c889324047fff125b19f856758c280be9401592
Gayle Martin <hello.gmartin@gmail.com>
2023-11-17 18:36:04 -0600

    Update form endpoint

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <div className='flex flex-col mb-10 mx-auto'>
       <div className='flex items-center justify-center'>
-        <form action={import.meta.env.VITE_ENDPOINT} method='POST' className='flex flex-col w-full md:w-7/12'>
+        <form action={import.meta.env.FORM_ENDPOINT} method='POST' className='flex flex-col w-full md:w-7/12'>
           <Title>Contact</Title>
           <input type='text' name='name' placeholder='Name' className='p-2 bg-transparent border-2 rounded-md focus:outline-none' />
           <input type='text' name='email' placeholder='Email' className='my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none' />
